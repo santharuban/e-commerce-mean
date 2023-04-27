ea8fba73e7c8e1f52a6b4579208b81110c5d5ade
santharuban <santhukalidass992@gmail.com>
2023-04-27 18:34:48 +0530

    middleware

---
+++ backend/app/middleware/authTokenVerify.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config;
+
+const authVerify = (role) => (req, res, next) => {
+  const accessToken = req.headers.authorization;
+
+  jwt.verify(accessToken, process.env.API_SECRET, (err, decoded) => {
+    if (err) {
+      res.status(401).json({ message: "Authorization token is not valid" });
+    } else {
+      const decode = jwt.decode(accessToken);
+      if (decode.role === role) {
+        next();
+      } else {
+        res.status(403).json({ message: "Access denied" });
+      }
+    }
+  });
+};
+const generateToken = (payload) => {
+  const token = jwt.sign(payload, process.env.API_SECRET, {});
+  return token;
+};
+
+
+
+module.exports = {
+  authTokenVerify: authVerify("Admin"),
+  authTokenVerifyUser: authVerify("user"),
+  generateToken
+};
